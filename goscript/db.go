585b60d341d42be2c3e5cef7c4363196007d7b39
Dhruv Jain <dhruvjain@Dhruvs-MacBook-Pro.local>
2020-06-01 15:12:39 -0700

    Pinging and retrieving mongodb cluster names as well as dep init setup properly.

--- goscript/db.go
+++ goscript/db.go
@@ -1,5 +1,36 @@
-package db
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://dbUser:dbUserPassword@cluster0-5nzly.mongodb.net/test?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
 }
